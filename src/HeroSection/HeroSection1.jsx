d58d0f5baa9c856fae8b63278dab5785e454243c
yash-199 <68182949+yash-199@users.noreply.github.com>
2025-02-24 23:58:45 +0530

    Hero Section 1 Add

--- src/HeroSection/HeroSection1.jsx
+++ src/HeroSection/HeroSection1.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react'
 import { FaRegEye, FaCode } from "react-icons/fa";
-import BackGroundImage from "../assets/HeroSection/HeroSection1.jpg"
-import { GoArrowUpRight } from "react-icons/go";
-import { IoCopy } from "react-icons/io5";
+import { FiMenu, FiSearch, FiUser } from "react-icons/fi"
+import HeroBanner from '../assets/Furniture.jpg'
+import { CiLocationArrow1 } from "react-icons/ci";
+import Chair from '../assets/Chair.png'
+import { CiHeart } from "react-icons/ci";
 const HeroSection1 = () => {
     const [showPreview, setShowPreview] = useState(true)
     return (
         <div>
-            <h1 className='font-bold my-10 text-3xl'>Hero Section 1</h1>
+            <h1 className='font-bold my-10 text-3xl'>Normal Tab</h1>
             <div className="py-4 px-4 mt-6 sm:py-6 sm:px-8 rounded-lg drop-shadow-lg w-full backdrop-blur-lg bg-[#1C1F26] border-2 border-[#343942] shadow-2xl">
                 {/* Header */}
                 <div className="flex justify-between items-center mb-4">
@@ -35,37 +37,96 @@ const HeroSection1 = () => {
                 </div>
                 {/* Content */}
                 <div className="bg-slate-900 rounded-lg overflow-hidden py-4 px-6">
-                    <div className="min-h-screen bg-gray-200 p-6 rounded-3xl mt-20 ">
-                        <div className="fixed rounded-3xl z-50 w-11/12">
-                            <div className="flex items-center justify-around text-black">
-                                <div className='bg-white rounded-full py-3 mt-1' >
-                                    <p className="font-bold text-3xl px-10">ReCom</p>
-                                </div>
-                                <ul className="flex items-center gap-5 text-sm mt-10">
-                                    <li className="cursor-pointer hover:text-gray-600">Shop</li>
-                                    <li className="cursor-pointer hover:text-gray-600">Browser</li>
-                                    <li className="cursor-pointer hover:text-gray-600">About</li>
-                                    <li className="cursor-pointer hover:text-gray-600">Blog</li>
-                                </ul>
-                                <div className="flex gap-4 cursor-pointer">
-                                    <p className='bg-white rounded-full text-1xl pt-3 px-6 hover:bg-[#f3f2f1] duration-200 transition-all'>Contact with us</p>
-                                    <GoArrowUpRight className='bg-white rounded-full p-2 text-5xl' />
+                    {/* Hero section */}
+                    <div className='relative h-[700px] rounded-3xl w-full bg-cover bg-center'
+                        style={{ backgroundImage: `url(${HeroBanner})` }}>
+                        {/* navbar */}
+                        <nav className='flex justify-between items-center shadow-md p-3'>
+                            {/* left side  */}
+                            <div className='flex items-center gap-4'>
+                                <button className='text-white bg-black text-2xl p-2 rounded-full'>
+                                    <FiMenu />
+                                </button>
+
+                                {/* navigation links */}
+                                <div>
+                                    <ul className='flex gap-2'>
+                                        <li className='bg-white text-black px-4 py-2 rounded-full text-base font-medium'>Home</li>
+                                        <li className='bg-gray-500 text-white px-4 py-2 rounded-full text-base font-medium'>Category</li>
+                                        <li className='bg-gray-500 text-white px-4 py-2 rounded-full text-base font-medium'>About</li>
+                                    </ul>
                                 </div>
                             </div>
-                        </div>
-                        <div
-                            className="relative h-[500px] rounded-3xl w-full bg-cover bg-center py-36 text-center text-white text-3xl font-bold mt-6"
-                            style={{ backgroundImage: `url(${BackGroundImage})` }}
-                        >
-                            <p className='text-black text-6xl'>Modern Furniture for</p>
-                            <p className='text-black text-6xl'>Minimalist Lovers</p>
-                            <p className='text-base font-normal text-black'>Experience the ultimate relaxation with our collection of serene <br /> and tranquil living-inspired design</p>
-                            <button className='flex items-center bg-black font-normal text-sm mx-auto my-10 pl-10 rounded-full py-1 gap-4'>Expolre More <GoArrowUpRight className='bg-white text-black rounded-full text-4xl p-2' /></button>
+                            {/* logo */}
+                            <h1 className='text-base font-bold'>Funiture.</h1>
+                            {/* Right side */}
+                            <div className='flex items-center gap-4'>
+                                <div className='flex items-center gap-2 bg-gray-500 px-3 py-1 rounded-full'>
+                                    <img className='w-8 h-8 rounded-full border-2 border-white' src="https://randomuser.me/api/portraits/women/44.jpg" alt="" />
+                                    <img className='w-8 h-8 rounded-full border-2 border-white -ml-2' src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />
+                                    <span className='text-base font-medium ml-2'>+15K Customers</span>
+                                </div>
+                                {/* search & Profile */}
+                                <FiSearch className='text-white text-3xl cursor-pointer bg-gray-500 p-2 rounded-full' />
+                                <FiUser className='text-white text-3xl cursor-pointer bg-black p-2 rounded-full' />
+                            </div>
+                        </nav>
+                        {/* content */}
+                        <div className='flex justify-between py-36 px-10'>
+                            <div>
+                                <p className='text-sm font-normal py-2'>Discover timeless pieces for every room in your <br /> home</p>
+                                <h1 className='text-4xl'>
+                                    Find the Perfect Balance of<br />
+                                    Comfort and Style with <b>Our <br />
+                                        Curated Collection.</b>
+                                </h1>
+                                <div className='bg-black/55 py-5 my-10 cursor-pointer hover:bg-black transition-all duration-200 text-center rounded-full w-3/12'>
+                                    <CiLocationArrow1 className='mx-auto text-2xl font-bold' />
+                                    <p className='text-sm font-light'>Go to the<br />
+                                        Online Store</p>
+                                </div>
+                            </div>
+                            <div className='flex justify-between items-center gap-4'>
+                                <div>
+                                    <div className='flex justify-between items-center bg-white/25 p-2 rounded-lg gap-4'>
+                                        <div className='bg-white/55 rounded-lg'>
+                                            <img src={Chair} className='w-32 h-32' alt="" />
+                                        </div>
+                                        <div>
+                                            <p className='text-base font-semibold'>High-Quality Furniture</p>
+                                            <p className='text-sm'>Discover timeless pieces for <br />
+                                                every room in your home</p>
+                                            <div className='flex gap-6 items-center'>
+                                                <button className='bg-white rounded-full text-sm text-black px-3 py-1.5 my-2'>
+                                                    Show Now
+                                                </button>
+                                                <CiHeart className='text-black cursor-pointer bg-white text-3xl p-2 rounded-full' />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='mt-2 flex justify-between items-center bg-white/25 p-2 rounded-lg gap-4'>
+                                        <div className='bg-white/55 rounded-lg'>
+                                            <img src={Chair} className='w-32 h-32' alt="" />
+                                        </div>
+                                        <div>
+                                            <p className='text-base font-semibold'>High-Quality Furniture</p>
+                                            <p className='text-sm'>Discover timeless pieces for <br />
+                                                every room in your home</p>
+                                            <div className='flex gap-6 items-center'>
+                                                <button className='bg-white rounded-full text-sm text-black px-3 py-1.5 my-2'>
+                                                    Show Now
+                                                </button>
+                                                <CiHeart className='text-black cursor-pointer bg-white text-3xl p-2 rounded-full' />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div >
+        </div>
     )
 }
 
